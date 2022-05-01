c62e2b50de2d340a92f7b6320f06864fec9a4f42
platisd <platisd@users.noreply.github.com>
2022-05-01 13:18:16 +0200

    Update pull request description instead of leaving a comment
    Let's update the pull request description instead
    since leaving a comment does not trigger a pull request
    workflow. A workaround could be to make a dummy change to
    the pull request description from an issue_comment workflow
    however that would require a Personal Access Token to be
    used for no good reason, which makes things more complex
    and potentially insecure.

--- run_action.py
+++ run_action.py
@@ -5,8 +5,6 @@ import sys
 import requests
 import argparse
 import yaml
-import json
-import time
 
 COMMENT_HEADER = os.environ["INPUT_COMMENT_HEADER"]
 EMPTY_CHECKMARK = "- [ ]"
@@ -19,23 +17,24 @@ def dod_criteria_to_message(dod_criteria):
     return message
 
 
-def get_bot_comment(comments):
-    for comment in comments:
-        if comment["user"]["type"] == "Bot" and COMMENT_HEADER in comment["body"]:
-            return comment["body"]
-    return ""
+def has_bot_comment(pull_request_description):
+    return COMMENT_HEADER in pull_request_description
 
 
-def has_unsatisfied_dod(bot_comment):
-    return EMPTY_CHECKMARK in bot_comment
+def has_unsatisfied_dod(pull_request_description, dod_criteria):
+    # Extract the bot message from the pull request description
+    bot_message_begin = pull_request_description.find(COMMENT_HEADER)
+    last_criterion = dod_criteria[-1]
+    bot_message_end = pull_request_description.find(last_criterion)
+    if bot_message_end == -1:
+        print(
+            "DoD criteria are missing. "
+            + "Please fully remove any remnants of the bot's comment and try again."
+        )
+        return False
+    bot_message = pull_request_description[bot_message_begin:bot_message_end]
 
-
-def is_pull_request_comment_edit_event(github_event):
-    return (
-        github_event["action"] == "edited"
-        and "issue" in github_event
-        and "pull_request" in github_event["issue"]
-    )
+    return EMPTY_CHECKMARK in bot_message
 
 
 def main():
@@ -64,28 +63,29 @@ def main():
 
     repo = os.environ.get("GITHUB_REPOSITORY")
     pull_request_id = args.pull_request_id
-
-    github_token = os.environ.get("INPUT_PAT_TOKEN")
+    github_token = os.environ.get("INPUT_GITHUB_TOKEN")
     github_api_url = os.environ.get("GITHUB_API_URL")
-    comments_url = "%s/repos/%s/issues/%s/comments" % (
+    authorization_header = {"authorization": "Bearer %s" % github_token}
+
+    issue_url = "%s/repos/%s/issues/%s" % (
         github_api_url,
         repo,
         pull_request_id,
     )
-
-    comments_get_result = requests.get(
-        comments_url, headers={"authorization": "Bearer %s" % github_token}
+    get_pull_request_description_result = requests.get(
+        issue_url, headers=authorization_header
     )
-    if comments_get_result.status_code != 200:
+    if get_pull_request_description_result.status_code != 200:
         print(
-            "Getting pull request comments to GitHub failed with code: "
-            + str(comments_get_result.status_code)
+            "Getting pull request description from GitHub failed with code: "
+            + str(get_pull_request_description_result.status_code)
         )
-        print(comments_get_result.text)
+        print(get_pull_request_description_result.text)
+        return 1
+    pull_request_description = get_pull_request_description_result.json()["body"]
 
-    bot_comment = get_bot_comment(comments_get_result.json())
-    if bot_comment:
-        if has_unsatisfied_dod(bot_comment):
+    if pull_request_description and has_bot_comment(pull_request_description):
+        if has_unsatisfied_dod(pull_request_description, config["dod"]):
             print(
                 "The Definition of Done for this pull request "
                 + "has not been yet been fully marked as satisfied "
@@ -96,79 +96,29 @@ def main():
             return 1
         else:
             print("All DoD criteria are satisfied. 🎉")
-            # If this action was triggered by a comment edit, we need to trigger the workflow
-            # via a pull request edit event, so that its status can be used for approving the pull request
-            github_event = {}
-            with open(os.environ["GITHUB_EVENT_PATH"]) as f:
-                github_event = json.load(f)
-
-            if not github_event:
-                print("No GitHub event found")
-                return 1
-
-            if is_pull_request_comment_edit_event(github_event):
-                # Update the pull request body and then set it back to the original state
-                issue_url = "%s/repos/%s/issues/%s" % (
-                    github_api_url,
-                    repo,
-                    pull_request_id,
-                )
-                get_comment_body_result = requests.get(
-                    issue_url, headers={"authorization": "Bearer %s" % github_token}
-                )
-                if get_comment_body_result.status_code != 200:
-                    print(
-                        "Getting pull request comment body from GitHub failed with code: "
-                        + str(get_comment_body_result.status_code)
-                    )
-                    print(get_comment_body_result.text)
-                    return 1
-                pull_request_original_body = get_comment_body_result.json()["body"]
-                pull_request_original_title = get_comment_body_result.json()["title"]
-
-                # Update the pull request body and title
-                update_comment_body_result = requests.patch(
-                    issue_url,
-                    headers={"authorization": "Bearer %s" % github_token},
-                    # Add a space to the original message which will be trimmed away by GitHub
-                    json={
-                        "title": pull_request_original_title + " wtf",
-                        "body": pull_request_original_body + " ignore this",
-                    },
-                )
-                if update_comment_body_result.status_code != 200:
-                    print(
-                        "Updating pull request comment body failed with code: "
-                        + str(update_comment_body_result.status_code)
-                    )
-                    print(update_comment_body_result.text)
-                    return 1
-
-                print("Updated pull request comment body")
-                return 0
-            else:
-                # If this is a pull request event we can succeed directly
-                return 0
+            return 0
     else:
+        bot_message = dod_criteria_to_message(config["dod"])
+        update_pull_request_description_result = requests.patch(
+            issue_url,
+            headers=authorization_header,
+            json={
+                "body": pull_request_description + "\n\n" + bot_message,
+            },
+        )
+        if update_pull_request_description_result.status_code != 200:
+            print(
+                "Updating pull request comment body failed with code: "
+                + str(update_pull_request_description_result.status_code)
+            )
+            return 1
+
         print(
             "The Definition of Done for this pull request "
-            + "has not been yet been fully marked as satisfied "
-            + "by a repository maintainer. "
+            + "needs to fully marked as satisfied by a repository maintainer. "
             + "Please make sure to check them off "
-            + "in the pull request comment that was just posted."
+            + "in the checklist that was appended to the pull request description."
         )
-        message = dod_criteria_to_message(config["dod"])
-        post_result = requests.post(
-            comments_url,
-            headers={"authorization": "Bearer %s" % github_token},
-            json={"body": message},
-        )
-        if post_result.status_code != 201:
-            print(
-                "Posting comment to GitHub failed with code: "
-                + str(post_result.status_code)
-            )
-            print(post_result.text)
         return 1
 
 
